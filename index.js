beb1f087d0ced9473c75d40b2c8757b5247b2438
Ruthuparnnan <ruthuparnnan123@gmail.com>
2023-10-07 12:07:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1.import json server
+const jsonServer = require("json-server");
+
+// 2.create our own server to run json file
+const server = jsonServer.create();
+
+// 3.generate a middleware to use in jsonserver
+const middleware = jsonServer.defaults();
+
+// 4.set up path for db.json
+const router = jsonServer.router("db.json");
+
+// 5.set up port for server
+const port = 3000 || process.env.PORT;
+
+// 7.use middleware,router,to server
+server.use(middleware);
+server.use(router);
+
+// 8.to listen / run to request
+server.listen(port, () => {
+  console.log("dont be a pssy");
+});
